44fb41a899e31e54fdb6fa093c7622f82c3a6582
adler99 <4921513+adler99@users.noreply.github.com>
2019-04-09 03:37:29 +0000

    package wide example

---
+++ example_test.go
@@ -0,0 +1,22 @@
+package quadtree_test
+
+import (
+	"github/noctilu/quadtree"
+)
+
+func Example() {
+	// empty tree with level 2
+	qt := quadtree.EmptyTree(2)
+
+	// quadtrees are immutable, so each change gives you a new quadtree
+	qt = qt.SetCell(1, 1, 1)
+
+	// print tree to console, don't do that for bigger trees
+	qt.Print()
+
+	// calculates next generation
+	qtNext := qt.NextGen()
+
+	qtNext.Print()
+	//output: as
+}
